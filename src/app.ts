dd0045ab25e2c8af14dd7f1e4d029b42968b972e
Chris Mayo <cmayo@microsoft.com>
2017-12-18 07:42:25 -0800

    Refactored rootTopic to be generic to follow pattern of Topic state.

--- src/app.ts
+++ src/app.ts
@@ -15,9 +15,13 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 const adapter = new BotFrameworkAdapter({ appId: process.env.MICROSOFT_APP_ID, appPassword: process.env.MICROSOFT_APP_PASSWORD });
 server.post('/api/messages', adapter.listen() as any);
 
+export interface RootTopicState<S> {
+    state?: S;
+}
+
 declare global {
     export interface ConversationState {
-        rootTopic?: ParentTopicState;
+        rootTopic?: RootTopicState<ParentTopicState>;
     }
 }
 
@@ -28,16 +32,16 @@ const bot = new Bot(adapter)
     .use(new BotStateManager())
     //.use(new LuisRecognizer("5e013df5-f1df-40a7-9893-437bfdb1811d", "04e545e56dfd417daa44460a04f20ffd"))
     .onReceive((context) => {
-        // State isn't fully initialized until the contact/conversation messages are sent, so have to require
-        //  activity type is message.
+        // TODO: State isn't fully initialized until the contact/conversation messages are sent, so have to require
+        //  activity type is message. Will affect welcome message. Refactor after bug has been addressed.
         if(context.request.type === 'message') {
-            // Initialize the root topic state and pass that reference to facilitate the state reference chain to 
-            //  context.state.conversation.
+            // Initialize the root topic state and pass that reference to root topic tofacilitate the state 
+            //  reference chain to context.state.conversation.
             if (!context.state.conversation.rootTopic) {
-                context.state.conversation.rootTopic = { activeTopic: undefined };
+                context.state.conversation.rootTopic.state = { activeTopic: undefined };
             }
 
-            const rootTopic = new RootTopic(context.state.conversation.rootTopic);
+            const rootTopic = new RootTopic(context.state.conversation.rootTopic.state);
             return rootTopic.onReceive(context);
         }
     });
