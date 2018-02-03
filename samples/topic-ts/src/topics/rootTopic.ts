e6e5b0bf99db0c45824ce0e57d87b94add0af047
Chris Mayo <cmayo@microsoft.com>
2018-02-03 11:12:34 -0800

    Renamed/refactored sample.

---
+++ samples/topic-ts/src/topics/rootTopic.ts
@@ -0,0 +1,44 @@
+import { TopicsRoot } from '../../../../source';
+import { Alarm } from '../alarms';
+import { AddAlarmTopic } from './addAlarmTopic';
+
+export class RootTopic extends TopicsRoot {
+
+    public constructor(context: BotContext) {
+        super(context);
+
+        this.subTopics
+            .set("addAlarmTopic", () => new AddAlarmTopic()
+                .onSuccess((context, value) => {
+                    this.clearActiveTopic();
+
+                    if (!context.state.user.alarms) {
+                        context.state.user.alarms = [];
+                    }
+                
+                    context.state.user.alarms.push({
+                        title: value.title,
+                        time: value.time
+                    });
+
+                    return context.reply(`Added alarm named '${ value.title }' set for '${ value.time }'.`);
+                })
+            )
+    }
+
+    public onReceive(context: BotContext) { 
+
+        if (context.request.type === 'message' && context.request.text.length > 0) {
+            if (/add alarm/i.test(context.request.text)) {
+
+                return this.setActiveTopic("addAlarmTopic").onReceive(context);
+            }         
+
+            return this.showDefaultMessage(context);
+        }
+    }
+
+    public showDefaultMessage(context: BotContext) {
+        context.reply("Say 'Add Alarm'.");
+    }
+}
