7966ead113293a7958c6aca101f77014cb289b18
Chris Mayo <cmayo@microsoft.com>
2018-02-10 14:10:07 -0800

    Added demo commented code.

--- samples/3-ConversationTopic-ts/src/topics/addAlarmTopic.ts
+++ samples/3-ConversationTopic-ts/src/topics/addAlarmTopic.ts
@@ -67,10 +67,12 @@ export class AddAlarmTopic extends ConversationTopic<AddAlarmTopicState, Alarm> 
 
     public onReceive(context: BotContext) {
 
+        // If there is an active topic, let it handle this turn until it completes.
         if(this.hasActiveTopic) { 
             return this.activeTopic.onReceive(context);
         }
 
+        // If the topic needs state to complete, set the active topic to prompt for it.
         if (!this.state.alarm.title) {
             return this.setActiveTopic("titlePrompt")
                 .onReceive(context);
@@ -100,3 +102,90 @@ class AlarmTimeValidator extends Validator<string> {
         return { value: context.request.text };
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
+            .set("titlePrompt", () => new Prompt<string>()
+                .onPrompt((context, lastTurnReason) => {
+   
+                    if(lastTurnReason && lastTurnReason === 'titletoolong') {
+                        context.reply(`Sorry, alarm titles must be less that 20 characters.`)
+                            .reply(`Let's try again.`);
+                    }
+    
+                    return context.reply(`What would you like to name your alarm?`);
+                })
+                .validator(new AlarmTitleValidator())
+                .maxTurns(2)
+                .onSuccess((context, value) => {
+                    this.clearActiveTopic();
+
+                    this.state.alarm.title = value;
+    
+                    return this.onReceive(context);
+                })
+                .onFailure((context, reason) => {                    
+                    this.clearActiveTopic();
+
+                    if(reason && reason === 'toomanyattempts') {
+                        context.reply(`I'm sorry I'm having issues understanding you.`);
+                    }
+    
+                    return this._onFailure(context, reason);
+                })
+            )
+            .set("timePrompt", () => new Prompt<string>()
+                .onPrompt((context, lastTurnReason) => {
+                    return context.reply(`What time would you like to set your alarm for?`);
+                })
+                .validator(new AlarmTimeValidator())
+                .maxTurns(2)
+                .onSuccess((context, value) => {
+                    this.clearActiveTopic();
+
+                    this.state.alarm.time = value;
+    
+                    return this.onReceive(context);
+                })
+                .onFailure((context, reason) => {
+                    this.clearActiveTopic();
+
+                    if(reason && reason === 'toomanyattempts') {
+                        return context.reply(`I'm sorry I'm having issues understanding you.`);
+                    }
+    
+                    return this._onFailure(context, reason);;
+                })
+            );
+            */
+
+        
+        /*
+        // If there is an active topic, let it handle this turn until it completes.
+        if(this.hasActiveTopic) { 
+            return this.activeTopic.onReceive(context);
+        }
+
+        // If the topic needs state to complete, set the active topic to prompt for it.
+        if (!this.state.alarm.title) {
+            return this.setActiveTopic("titlePrompt")
+                .onReceive(context);
+        } 
+        
+        if (!this.state.alarm.time) {
+            return this.setActiveTopic("timePrompt")
+                .onReceive(context);
+        }
+        
+        return this._onSuccess(context, this.state.alarm);
+        */
