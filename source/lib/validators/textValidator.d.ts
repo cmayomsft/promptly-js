dfb65e2c0500e1dc96453bc00137c726ab3d46a7
Chris Mayo <cmayomsft@users.noreply.github.com>
2018-05-07 07:07:51 -0700

    Old *.d.ts that wasn't commited for some reason.

--- source/lib/validators/textValidator.d.ts
+++ source/lib/validators/textValidator.d.ts
@@ -2,7 +2,7 @@ import { Validator } from '../validators/validator';
 import { BotContext } from 'botbuilder';
 export declare class TextValidator<BotTurnContext extends BotContext> extends Validator<BotTurnContext, string> {
     validate(context: BotTurnContext): {
-        value: string;
+        value: any;
         reason?: undefined;
     } | {
         reason: string;
