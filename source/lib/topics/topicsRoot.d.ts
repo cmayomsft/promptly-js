491a8758691e6f3cf8ef0b1dc6be93f0f10e2bb0
Chris Mayo <cmayomsft@users.noreply.github.com>
2018-05-08 08:32:51 -0700

    Updated to m4.2

--- source/lib/topics/topicsRoot.d.ts
+++ source/lib/topics/topicsRoot.d.ts
@@ -1,9 +1,9 @@
 import { ConversationTopic, ConversationTopicState } from './conversationTopic';
-import { BotContext } from 'botbuilder';
+import { TurnContext } from 'botbuilder';
 export interface PromptlyBotConversationState<RootTopicState extends ConversationTopicState> {
     rootTopic?: RootTopicState;
 }
-export interface PromptlyBotTurnContext<BotConversationState extends PromptlyBotConversationState<RootTopicState>, RootTopicState extends ConversationTopicState> extends BotContext {
+export interface PromptlyBotTurnContext<BotConversationState extends PromptlyBotConversationState<RootTopicState>, RootTopicState extends ConversationTopicState> extends TurnContext {
     conversationState: BotConversationState;
 }
 export declare abstract class TopicsRoot<BotTurnContext extends PromptlyBotTurnContext<BotConversationState, RootTopicState>, BotConversationState extends PromptlyBotConversationState<RootTopicState>, RootTopicState extends ConversationTopicState> extends ConversationTopic<BotTurnContext, RootTopicState> {
