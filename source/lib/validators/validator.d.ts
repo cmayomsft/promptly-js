f7f2dfbafae7344d5807e0477bffb5f6b358f378
Chris Mayo <cmayomsft@users.noreply.github.com>
2018-05-08 08:26:26 -0700

    Updated to M4.2

--- source/lib/validators/validator.d.ts
+++ source/lib/validators/validator.d.ts
@@ -1,8 +1,8 @@
-import { BotContext } from 'botbuilder';
+import { TurnContext } from 'botbuilder';
 export interface ValidatorResult<Value> {
     value?: Value;
     reason?: string;
 }
-export declare abstract class Validator<BotTurnContext extends BotContext, Value> {
+export declare abstract class Validator<BotTurnContext extends TurnContext, Value> {
     abstract validate(context: BotTurnContext): ValidatorResult<Value>;
 }
