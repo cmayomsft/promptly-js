688ee994dea34349187c4c1f76c5e5f4763e7185
Chris Mayo <cmayomsft@users.noreply.github.com>
2018-05-08 08:21:23 -0700

    Commiting changes to *.d.ts that I missed.

--- source/lib/topics/conversationTopic.d.ts
+++ source/lib/topics/conversationTopic.d.ts
@@ -1,4 +1,4 @@
-import { BotContext } from 'botbuilder';
+import { TurnContext } from 'botbuilder';
 import { Topic } from './topic';
 export interface ActiveTopicState {
     key: string;
@@ -7,7 +7,7 @@ export interface ActiveTopicState {
 export interface ConversationTopicState {
     activeTopic?: ActiveTopicState;
 }
-export declare abstract class ConversationTopic<BotTurnContext extends BotContext, State extends ConversationTopicState, Value = any> extends Topic<BotTurnContext, State, Value> {
+export declare abstract class ConversationTopic<BotTurnContext extends TurnContext, State extends ConversationTopicState, Value = any> extends Topic<BotTurnContext, State, Value> {
     private _subTopics;
     protected readonly subTopics: Map<string, (any?) => Topic<BotTurnContext, any>>;
     private _activeTopic;
