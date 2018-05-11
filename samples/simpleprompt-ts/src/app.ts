01016057e9e5cbb1e4672a71973d32640467b315
Chris Mayo <cmayomsft@users.noreply.github.com>
2018-05-11 07:39:32 -0700

    Removed un-needed message type check.

--- samples/simpleprompt-ts/src/app.ts
+++ samples/simpleprompt-ts/src/app.ts
@@ -4,16 +4,14 @@ import { RootTopic , RootTopicState } from './topics/rootTopic';
 
 // Define conversation state shape
 export interface BotConversationState extends PromptlyBotConversationState<RootTopicState> { }
+
+// Define user state shape
 export interface BotUserState { }
 
 const simplePromptBot = new ServiceBot<BotConversationState, BotUserState>();
 
 simplePromptBot.onTurn(async turnContext => {
-    // State isn't fully initialized until the contact/conversation messages are sent, so have to require
-    //  activity type is message. Will affect welcome message. Refactor after bug has been addressed.
-    if(turnContext.activity.type === 'message') {
-        
-        return new RootTopic(turnContext)
-            .onReceiveActivity(turnContext);
-    }
+
+    return new RootTopic(turnContext)
+        .onReceiveActivity(turnContext);
 });
