3bdaaf66ec3d31e8be93dbff9705d14b67e65a8d
Chris Mayo <cmayomsft@users.noreply.github.com>
2018-06-01 07:25:34 -0700

    Updated ConversationTopic based on change to strict type checking.

--- source/lib/topics/conversationTopic.d.ts
+++ source/lib/topics/conversationTopic.d.ts
@@ -9,10 +9,10 @@ export interface ConversationTopicState {
 }
 export declare abstract class ConversationTopic<BotTurnContext extends TurnContext, State extends ConversationTopicState, Value = any> extends Topic<BotTurnContext, State, Value> {
     private _subTopics;
-    protected readonly subTopics: Map<string, (any?) => Topic<BotTurnContext, any>>;
-    private _activeTopic;
+    protected readonly subTopics: Map<string, (...args: any[]) => Topic<BotTurnContext, any>>;
+    private _activeTopic?;
     setActiveTopic(subTopicKey: string, ...args: any[]): Topic<BotTurnContext, any, any>;
-    readonly activeTopic: Topic<BotTurnContext, any>;
+    readonly activeTopic: Topic<BotTurnContext, any> | undefined;
     readonly hasActiveTopic: boolean;
     clearActiveTopic(): void;
 }
--- source/src/topics/conversationTopic.ts
+++ source/src/topics/conversationTopic.ts
@@ -26,15 +26,15 @@ export abstract class ConversationTopic<BotTurnContext extends TurnContext, Stat
 
     // subTopics - Map of functions used to create any sub-topics for the conversation topic
     //  between turns.
-    private _subTopics = new Map<string, (any?) => Topic<BotTurnContext, any>>();
-    protected get subTopics(): Map<string, (any?) => Topic<BotTurnContext, any>> {
+    private _subTopics = new Map<string, (...args: any[]) => Topic<BotTurnContext, any>>();
+    protected get subTopics(): Map<string, (...args: any[]) => Topic<BotTurnContext, any>> {
         return this._subTopics;
     }
 
     // _activeTopic - The "active" Topic for the ConversationTopic. When one of the sub-topics is the 
     //  active topic, the ConversationTopic's onReceive() should call/defer to the active Topic's 
     //  onReceive() to handle messages from the user until the active Topic completes.
-    private _activeTopic: Topic<BotTurnContext, any>;
+    private _activeTopic?: Topic<BotTurnContext, any>;
     
     // setActiveTopic - Called to set one of the sub-topics managed by this.subTopics() to be 
     //  the active Topic. 
@@ -42,14 +42,21 @@ export abstract class ConversationTopic<BotTurnContext extends TurnContext, Stat
     //      on the initial turn (turn 0) and to recreate the active topic on subsequent turns, 
     //      until the active Topic completes.
     //  args - Any arguments used to create the topic on the initial turn (turn 0).
-    public setActiveTopic(subTopicKey: string, ...args) {
+    public setActiveTopic(subTopicKey: string, ...args: any[]) {
 
-        // Instantiate/set the active Topic by calling the corresponding function in this.subTopics(),
+        let subtopic = this.subTopics.get(subTopicKey);
+
+        // Check to make sure there is a function to create the Topic in this.subTopics.
+        if (subtopic === undefined) {
+            throw new Error(`There is no subtopic with the key '${ subTopicKey }'. Could '${ subTopicKey }' be misspelled?`);
+        }
+
+        // Instantiate/set the active Topic by calling the corresponding function from this.subTopics(),
         //  using args if supplied.
         if(args.length > 0) {
-            this._activeTopic = this.subTopics.get(subTopicKey)(...args);;
+            this._activeTopic = subtopic(...args);;
         } else {
-            this._activeTopic = this.subTopics.get(subTopicKey)();;
+            this._activeTopic = subtopic();;
         }
 
         // Persist the this.subTopics() key used to create/set the active Topic and the 
@@ -62,7 +69,7 @@ export abstract class ConversationTopic<BotTurnContext extends TurnContext, Stat
     
     // activeTopic - Used to recreate ("rehydrate") the active topic on the current turn
     //  so it can handle the context/message of the current turn.
-    public get activeTopic(): Topic<BotTurnContext, any> {
+    public get activeTopic(): Topic<BotTurnContext, any> | undefined {
 
         // If there is no active topic persisted in state, there is not active topic.
         if(!this.state.activeTopic) {
@@ -74,9 +81,17 @@ export abstract class ConversationTopic<BotTurnContext extends TurnContext, Stat
             return this._activeTopic;
         }
 
+        let subtopic = this.subTopics.get(this.state.activeTopic.key);
+
+        // Check to make sure there is a function to create the Topic in this.subTopics.
+        if (subtopic === undefined) {
+            throw new Error(`Could not find an activeTopic with the key '${ this.state.activeTopic.key }'. Could '${ this.state.activeTopic.key }' have been removed from subTopics?`);
+        }
+
         // Recreate the active topic using the applicable function in this.subTopics() 
         //  and the state persisted on the last turn.
-        this._activeTopic = this.subTopics.get(this.state.activeTopic.key)();
+        this._activeTopic = subtopic();
+
         this._activeTopic.state = this.state.activeTopic.state;
         
         return this._activeTopic;
